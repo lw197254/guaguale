6868cecda3bbc68bb35ec87c5da02950b6c20b94
刘伟 <liuwei@liuwei.local>
2016-10-23 13:09:52 +0800

    aaa

--- Day12-幸运转盘/Day12-幸运转盘/LoginViewController.m
+++ Day12-幸运转盘/Day12-幸运转盘/LoginViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    ///zhendebuzhidao zenmeban
     // Do any additional setup after loading the view from its nib.
 }
 - (IBAction)backClicked:(UIButton *)sender {
